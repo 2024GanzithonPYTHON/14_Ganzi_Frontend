262cf2311a3089faed0f8413244f47f8da15f6a5
igbinni143 <bin143@naver.com>
2024-11-16 20:33:33 +0900

    🐛 Fix: Header 탭 반응 크기 수정

--- src/styles/LoginAfterHeaderStyle.js
+++ src/styles/LoginAfterHeaderStyle.js
@@ -4,18 +4,18 @@ export const Header = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	padding: 0 200px;
+	padding: 0 clamp(10px, 5vw, 100px);
 	height: 80px;
 `;
 
 export const Logo = styled.h1`
 	font-family: 'Pretendard', sans-serif;
-	font-size: 32px;
+	font-size: clamp(1.5vw, 2vw, 32px);
 	font-style: normal;
 	font-weight: 800;
 	line-height: normal;
+	left: clamp(10px, 2vw, 50px);
 	color: ${({ theme }) => theme.color.primary};
-
 	cursor: pointer;
 `;
 
@@ -29,21 +29,24 @@ export const Tab = styled.div`
 	color: ${({ theme, $active }) =>
 		$active ? theme.color.primary : theme.color.text};
 	font-family: 'Pretendard', sans-serif;
-	font-size: 18px;
+	font-size: clamp(14px, 1.5vw, 18px); /* 최소 14px, 기본 1.5vw, 최대 18px */
 	font-style: normal;
 	font-weight: 600;
 	line-height: normal;
-	margin-right: 63px;
+	margin-right: clamp(20px, 4vw, 63px);
 	cursor: pointer;
+	white-space: nowrap;
+	overflow: hidden;
 `;
 
 export const Tab2 = styled.div`
 	color: ${({ theme }) => theme.color.gray4};
 	font-family: 'Pretendard', sans-serif;
-	font-size: 18px;
+	font-size: clamp(14px, 1.5vw, 18px);
 	font-style: normal;
 	font-weight: 600;
 	line-height: normal;
 	cursor: pointer;
+	white-space: nowrap;
+	overflow: hidden;
 `;
-
