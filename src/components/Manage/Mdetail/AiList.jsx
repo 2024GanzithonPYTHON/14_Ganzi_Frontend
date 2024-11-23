f8fcd633428fd1a43afdfa7160d6ef06d44bd4ad
igbinni143 <bin143@naver.com>
2024-11-23 16:08:06 +0900

    123

--- src/components/Manage/Mdetail/AiList.jsx
+++ src/components/Manage/Mdetail/AiList.jsx
@@ -5,22 +5,7 @@ const AiList = ({ aiSummaries }) => {
 	return (
 		<div>
 			<h1>AI 요약 목록</h1>
-			{aiSummaries && aiSummaries.length > 0 ? (
-				<ul>
-					{aiSummaries.map((summary, index) => (
-						<li key={index}>
-							<AiListperson>
-								<strong>주제:</strong> {summary.message}
-							</AiListperson>
-							<AiListGPT>
-								<strong>요약:</strong> {summary.result}
-							</AiListGPT>
-						</li>
-					))}
-				</ul>
-			) : (
-				<p>요약된 내용이 없습니다.</p>
-			)}
+			{}
 		</div>
 	);
 };
